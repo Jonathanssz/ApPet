09c60b9e46b4715d680b2d386efbbbb5b03debea
Jonathan Souza <jonathanss400@hotmail.com>
2022-08-21 00:54:23 -0300

    Terminando a configuracao e testando database

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,24 @@ import { Injectable } from '@angular/core';
 })
 export class DatabaseService {
 
-  constructor() { }
+  private apiUrl = 'https://jsonplaceholder.typicode.com/posts'
+
+  constructor(public http: HttpClient) { }
+
+  getInformacao() {
+    return new Promise( (resolve, reject) => {
+      this.http.request('GET', this.apiUrl, {responseType: 'json'}).subscribe(
+        (result: any) => {
+          console.log('>>> Utilizando HttpClient <<<');
+          console.log(result);
+          resolve(result.json);
+        },
+        error => {
+          console.error(error);
+          reject(error.json);
+        }
+      );
+    }).catch(e => console.error(e));
+  }
+
 }
